f32235b97394438703428c1cdb033d8b6472ac11
alexander <oulfc93@gmail.com>
2021-06-18 22:56:20 +0300

    build webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const heading = document.querySelector('h1');
+heading.textContent = 'RSS агрегатор';
+
+const root = document.querySelector('body');
+root.append(heading);
+
+const formBase = document.createElement('form');
+root.append(formBase);
+
+const inputField = document.createElement('input');
+formBase.append(inputField);
+
+const buttonSubmit = document.createElement('button');
+buttonSubmit.classList.add('btn', 'btn-primary');
+buttonSubmit.textContent = 'Добавить';
+formBase.append(buttonSubmit);
