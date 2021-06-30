2163a46297aecf1ec3445224f54383b024c02daa
alexander <oulfc93@gmail.com>
2021-06-30 20:08:20 +0300

    feeds list

--- src/index.js
+++ src/index.js
@@ -41,14 +41,16 @@ const state = {
         errors: {},
     },
     dataRss: {        
-        feed: [],
-        post: [],
+        feeds: [],
+        posts: [],
     },
 }
 
 const formSubmit = document.querySelector('.rss-form');
+const submitButton = document.querySelector('button[type="submit"]');
 const inputUrl = document.querySelector('#urlInput');
 const divURL = document.querySelector('#divUrl');
+const divFeeds = document.querySelector('.feeds');
 
 const renderErrors = (element, errors) => { //refactoring
   const elementError =  element;
@@ -72,20 +74,58 @@ const renderErrors = (element, errors) => { //refactoring
 const processStateHandler = (processState) => {
   switch (processState) {
     case 'failed':
-      //submitButton.disabled = false;
+      submitButton.disabled = false;
       break;
      case 'filling':
     //   submitButton.disabled = false;
       break;
-     case 'accepted':
-    //   submitButton.disabled = true;
-       break;
-    // case 'finished':
-    //   container.innerHTML = 'User Created!';
-    //   break;
+    case 'accepted':
+      submitButton.disabled = false;
+      break;
+    case 'sending':
+      submitButton.disabled = true;
+      break;
     default:
       throw new Error(`Unknown state: ${processState}`);
   }
+};
+
+const renderFeeds = (feeds, rootFeeds) => {
+  rootFeeds.innerHTML = '';
+  const cardFeeds = document.createElement('div');
+  cardFeeds.classList.add('card', 'border-0');
+  rootFeeds.appendChild(cardFeeds);
+  const bodyFeeds = document.createElement('div');
+  bodyFeeds.classList.add('card-body');
+  cardFeeds.appendChild(bodyFeeds);
+  const titleFeeds = document.createElement('h2');
+  titleFeeds.classList.add('card-title');
+  titleFeeds.textContent = 'Feeds';
+  bodyFeeds.appendChild(titleFeeds);
+  const groupListFeeds = document.createElement('ul');
+  groupListFeeds.classList.add('list-group');
+  cardFeeds.appendChild(groupListFeeds);
+
+  feeds.forEach(({title, description }) => {
+    console.log(`TITO ${title}`);
+    console.log(`TITO ${description}`);
+      
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item', 'border-0');
+    groupListFeeds.appendChild(listItem);
+
+    const titleItem = document.createElement('h3');
+    titleItem.classList.add('m-0', 'h6');
+    titleItem.textContent = title;
+    listItem.appendChild(titleItem);
+
+    const descrItem = document.createElement('p');
+    descrItem.classList.add('m-0', 'small', 'text-black-50');
+    descrItem.innerHTML = description;
+    listItem.appendChild(descrItem);
+    
+  });
+
 };
 
 const watchedState = onChange(state, (path, value) => {
@@ -102,6 +142,17 @@ const watchedState = onChange(state, (path, value) => {
       case 'inputRSSForm.process':
         processStateHandler(value);
         break;
+      case 'inputRSSForm.process':
+        processStateHandler(value);
+        break;
+      case 'dataRss.feeds':        
+        //console.log('feeds');
+        renderFeeds(value, divFeeds);
+        break;
+      case 'dataRss.posts':
+        console.log('posts');
+        //renderPosts(value);
+        break;  
       default:
         break;
     }
@@ -109,41 +160,42 @@ const watchedState = onChange(state, (path, value) => {
 });
 
 const updateValidationState = (watchedState) => {
-    const errors = validateURL(watchedState.inputRSSForm.data);
-    watchedState.inputRSSForm.valid = _.isEqual(errors, {});
-    watchedState.inputRSSForm.errors = errors;
+  const errors = validateURL(watchedState.inputRSSForm.data);
+  watchedState.inputRSSForm.valid = _.isEqual(errors, {});
+  watchedState.inputRSSForm.errors = errors;
+};
+
+const writeData = (data, currentUrl) => {
+  const repeatUrl = watchedState.dataRss.feeds.filter((el) => el.url === currentUrl);
+  if (!_.isEqual(repeatUrl, [])) {
+    throw new Error(`RSS уже существует`);
+  }
+  const {title, description, items} = data;  
+  console.log(items);
+  const id = _.uniqueId();
+  watchedState.dataRss.feeds.push({id: id, title: title, description: description, url: currentUrl });
+  watchedState.dataRss.posts.push({id: id, posts: items });
 };
 
 formSubmit.addEventListener('submit', (e) => {
    e.preventDefault();
    const formData = new FormData(e.target);
    const url = formData.get('url');   
-   watchedState.inputRSSForm.data.url = url;
-   updateValidationState(watchedState);   
+   watchedState.inputRSSForm.data.url = url; //delete this
+   updateValidationState(watchedState);
+   watchedState.inputRSSForm.process = 'sending';
    axios.get(`${proxyServ}${encodeURIComponent(url)}`)
      .then((response) => {
-       // handle success
-       //console.log(response.data.contents);       
-       //watchedState.inputRSSForm.process = 'accepted';       
-       const content = parser(response.data.contents, 'dom'); 
-       return;      
-       const rssC = content.querySelector('rss');
-       if (rssC) {
-        const titleContent = content.querySelector('title').textContent;
-        console.log(rssC);
-        console.log(`title eq = ${titleContent}`);
-        watchedState.dataRss.feed.push({ id:_.uniqueId(), feedName: titleContent} );
-        watchedState.inputRSSForm.process = 'accepted';
-        return;
-       }
-       watchedState.inputRSSForm.processError = errorMessages.parsing.error;
-       watchedState.inputRSSForm.process = 'failed';
-       // parsing rss - case 1: nice rss, get feeds; case 2: bad rss, get error
+       // handle success     
+      const content = parser(response.data.contents);
+      console.log(content);       
+      writeData(content, url);
+      watchedState.inputRSSForm.process = 'accepted';   //success info?   
      })
      .catch((error) => {
 //       // handle error
+      console.log(`handle error ${error}`);
        watchedState.inputRSSForm.processError = errorMessages.network.error;
-       watchedState.inputRSSForm.process = 'failed';
-       console.log(error);
+       watchedState.inputRSSForm.process = 'failed';     
      });
  });
--- src/parser.js
+++ src/parser.js
@@ -2,10 +2,24 @@
 export default (data) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(data, 'application/xml');
-  console.log(doc);
+  //console.log(doc);
   const parseError = doc.querySelector('parsererror');
   if (parseError) {
     throw new Error(`Parse error: ${parseError}`);
-  }  
-  return doc;
+  }
+  console.log('after throw');
+  const result = {};
+  const title = doc.querySelector('title').textContent;
+  result.title = title;
+  result.description = doc.querySelector('description').textContent;
+  const items = [...doc.querySelectorAll('item')];
+  const mappedItems = items.map((i) => ({title: i.querySelector('title').textContent, 
+  description: i.querySelector('description').textContent,
+  link: i.querySelector('link').textContent
+  }))
+  //.map((el) => el.textContent);
+  result.items = mappedItems;
+  console.log(`Title ${result.title}`);
+  console.log(`Description ${result.description}`);
+  return result;
 };
